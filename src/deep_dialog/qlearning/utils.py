c1111888edf95c859f1b684927a3113e26ff8b7c
Haley <gogogo753951741963@gmail.com>
2017-05-11 12:35:34 +0800

    [Modify] fix sytax convnetion

--- src/deep_dialog/qlearning/utils.py
+++ src/deep_dialog/qlearning/utils.py
@@ -9,9 +9,9 @@ import math
 
 
 def initWeight(n, d):
-    scale_factor = math.sqrt(float(6)/(n + d))
-    #scale_factor = 0.1
-    return (np.random.rand(n,d)*2-1)*scale_factor
+    scale_factor = math.sqrt(float(6) / (n + d))
+    # scale_factor = 0.1
+    return (np.random.rand(n, d) * 2 - 1) * scale_factor
 
 """ for all k in d0, d0 += d1 . d's are dictionaries of key -> numpy array """
 def mergeDicts(d0, d1):
